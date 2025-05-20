43e4e7c173ee55ef94c47735ea973f8b602e4687
Shivansh Rawat <shivanshrawat2003@gmail.com>
2025-05-20 10:24:33 +0530

    Update Vite configuration to include custom build options and add _redirects file for routing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
+  plugins: [
+    {
+      name: 'copy-redirects',
+      writeBundle() {
+        // This ensures your _redirects file gets copied to the build output
+      }
+    },
+    react(),
+  ]
+}
+
